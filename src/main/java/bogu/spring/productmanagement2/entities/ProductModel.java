ceb1ab12d5d2e8c251d232b85fbe61b708764a18
Bogdan Catana <catanabogdan23@yahoo.com>
2021-05-08 10:28:39 +0300

    create models product and manufacturer

---
+++ src/main/java/bogu/spring/productmanagement2/entities/ProductModel.java
@@ -0,0 +1,56 @@
+package bogu.spring.productmanagement2.entities;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "products")
+public class ProductModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    private String description;
+    private double price;
+    private String image;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+}
