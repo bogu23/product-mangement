1de9026d68378d681b2649554dc9bb20797674ff
Bogdan Catana <catanabogdan23@yahoo.com>
2021-05-08 11:55:46 +0300

    almost done....add methods in ProductController

---
+++ src/main/java/bogu/spring/productmanagement2/controller/ProductController.java
@@ -0,0 +1,47 @@
+package bogu.spring.productmanagement2.controller;
+
+import bogu.spring.productmanagement2.entities.ProductModel;
+import bogu.spring.productmanagement2.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class ProductController {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @GetMapping("getProducts")
+    public List<ProductModel> getAllProducts() {
+        List<ProductModel> productModelList = productRepository.findAll();
+        return productModelList;
+    }
+
+    @PostMapping("addProducts")
+    public void addProduct(@RequestBody ProductModel productToBeAdded) {
+        productRepository.save(productToBeAdded);
+    }
+
+    @PutMapping("edit")
+    public void editProduct(@RequestBody ProductModel productToBeEdited) {
+        productRepository.save(productToBeEdited);
+
+    }
+
+    @GetMapping("getProductById/{id}")
+    public ProductModel getProductById(@PathVariable long id) {
+       Optional<ProductModel> foundProduct = productRepository.findById(id);
+        return foundProduct.get();
+    }
+
+    @DeleteMapping("deleteById/{id}")
+    public void deleteProductById(@PathVariable long id) {
+        productRepository.deleteById(id);
+    }
+
+
+
+}
--- src/main/java/bogu/spring/productmanagement2/repository/ProductRepository.java
+++ src/main/java/bogu/spring/productmanagement2/repository/ProductRepository.java
@@ -1,2 +1,11 @@
-package bogu.spring.productmanagement2.repository;public interface ProductRepository {
+package bogu.spring.productmanagement2.repository;
+
+import bogu.spring.productmanagement2.entities.ProductModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<ProductModel, Long> {
+
+
 }
