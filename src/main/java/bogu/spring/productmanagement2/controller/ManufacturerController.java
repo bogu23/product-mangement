c993578ab8a115dfd86b3110d2008128ee5e0388
Bogdan Catana <catanabogdan23@yahoo.com>
2021-05-08 21:26:20 +0300

    add method getManufacturerById in manufacturerController

--- src/main/java/bogu/spring/productmanagement2/controller/ManufacturerController.java
+++ src/main/java/bogu/spring/productmanagement2/controller/ManufacturerController.java
@@ -3,9 +3,12 @@ package bogu.spring.productmanagement2.controller;
 import bogu.spring.productmanagement2.entities.ManufacturerModel;
 import bogu.spring.productmanagement2.repository.ManufacturerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class ManufacturerController {
@@ -13,8 +16,15 @@ public class ManufacturerController {
     @Autowired
     private ManufacturerRepository manufacturerRepository;
 
+    @GetMapping("getManufacturers")
     public List<ManufacturerModel> getAllManufacturers() {
         List<ManufacturerModel> foundManufacturers = manufacturerRepository.findAll();
         return foundManufacturers;
+    }
+
+    @GetMapping("getManufacturerById/{id}")
+    public ManufacturerModel getManufacturerById(@PathVariable long id) {
+        Optional<ManufacturerModel> foundManufacturer = manufacturerRepository.findById(id);
+        return foundManufacturer.get();
     }
 }
