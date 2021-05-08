7213def1b16105f9c6d60af3c97678c5d21b498f
Bogdan Catana <catanabogdan23@yahoo.com>
2021-05-08 10:21:16 +0300

    start project and set the db connect

---
+++ src/main/java/bogu/spring/productmanagement2/Productmanagement2Application.java
@@ -0,0 +1,13 @@
+package bogu.spring.productmanagement2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Productmanagement2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Productmanagement2Application.class, args);
+	}
+
+}
---
+++ src/test/java/bogu/spring/productmanagement2/Productmanagement2ApplicationTests.java
@@ -0,0 +1,13 @@
+package bogu.spring.productmanagement2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Productmanagement2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
